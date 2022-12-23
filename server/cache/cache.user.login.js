2b573339b4f86130a1bb4f420bf73de49f97f1db
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2022-12-23 14:39:57 -0500

    Update cache.user.login.js

--- server/cache/cache.user.login.js
+++ server/cache/cache.user.login.js
@@ -6,22 +6,22 @@ const generate_token = require("../config/token");
 
 const findUserFromLoginInCache = async (email, password) => {
     const runUserValidation = await validateVisitorLoginInput(email, password);
+    
     if (runUserValidation.isValidLoginInput == false) {return}
 
     const cacheEngine = (await currentCacheEngine()).client
     const findUserInCache = await cacheEngine.get(email);
 
-    // if (findUserInCache) {
-    //     console.log('from cache <---------');
-    //     user = JSON.parse(findUserInCache);
-    //     return {msg: loginValidationHelpers.successfulLogin, user: user, token:generate_token(user._id)}
-    // }
+    if (findUserInCache) {
+        user = JSON.parse(findUserInCache);
+        return {msg: loginValidationHelpers.successfulLogin, user: user, token:generate_token(user._id)}
+    }
 
     const authorizeUser = await authorizeUserToStartGame(email, password)
     if (authorizeUser.user) {const setUserInCache = await cacheEngine.set(email, JSON.stringify(authorizeUser.user))}
     return {authorizeUser}
     
-    // return {user: authorizeUser.user, token: authorizeUser.token, msg: authorizeUser.loginSucceded}}
+    return {user: authorizeUser.user, token: authorizeUser.token, msg: authorizeUser.loginSucceded}}
 }
 
 module.exports = findUserFromLoginInCache
